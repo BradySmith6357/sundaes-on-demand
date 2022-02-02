60be605a48993decd2c12e9bf398a8afd2ca8c24
bqsmith <bqsmith@businessolver.com>
2022-02-02 15:26:39 -0700

    Added mock servive worker

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,14 +1,27 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import {useState} from "react";
+import {Popover} from "react-bootstrap";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 
 export default function SummaryForm() {
     const [tcChecked, setTcChecked] = useState(false);
 
+    const popover = (
+        <Popover id="termsandconditions-popover">
+            <Popover.Body>
+                No ice cream will actually be delivered
+            </Popover.Body>
+        </Popover>
+    );
+
     const checkboxLabel = (
         <span>
-            I agree to <span stile={{color: 'blue'}}> Terms and Conditions </span>
+            I agree to
+            <OverlayTrigger placement="right" overlay={popover}>
+                <span style={{ color: 'blue' }}> Terms and Conditions </span>
+            </OverlayTrigger>
         </span>
     );
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import {queryByText, render, screen, waitForElementToBeRemoved} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test('Checkbox is unchecked by default and button is disabled', () => {
     render(<SummaryForm />);
@@ -17,11 +18,30 @@ test('Checking checkbox enables button', () => {
     const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions'});
     const confirmButton = screen.getByRole('button', {name: 'Confirm order'})
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect(checkbox).toBeChecked();
     expect(confirmButton).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    userEvent.click(checkbox);
     expect(confirmButton).toBeDisabled();
 
 })
+
+test('popover response to hover', async() => {
+    render(<SummaryForm />);
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears upon mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsAndConditions);
+    const popover = screen.getByText(/no ice cream will actually be delivered/i);
+    expect(popover).toBeInTheDocument();
+
+    //popover disappears when we mouse out
+    userEvent.unhover(termsAndConditions);
+    await waitForElementToBeRemoved(() =>
+        screen.queryByText(/no ice cream will actually be delivered/i)
+    );
+});
